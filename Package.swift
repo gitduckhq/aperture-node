3bd43c013e566516efe9cc3581f1d9070a543418
Drag0s <me@dragosfotescu.com>
2019-05-14 17:04:09 +0200

    Change depencencies: wulkano/Aperture by gitduckhq/Aperture

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/wulkano/Aperture", from: "0.2.0")
+    .package(url: "https://github.com/gitduckhq/Aperture", .branch("master"))
   ],
   targets: [
     .target(
